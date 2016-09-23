64e62d67352053c2347c970f819cccfbff4935e5
Holly Bergfors <hrbergfors@gmail.com>
2016-09-23 12:51:35 -0700

    Add static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,11 @@ var jsSources = [
 
 var sassSources = ['components/sass/style.scss'];
 
+var htmlSources = ['builds/development/*.html'];
+
+var jsonSources = ['builds/development/js/*.json'];
+
+
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true })
@@ -44,10 +49,14 @@ gulp.task('compass', function() {
 		.pipe(connect.reload())
 });
 
-gulp.task('watch', function() {
-	gulp.watch(coffeeSources, ['coffee']);
-	gulp.watch(jsSources, ['js']);
-	gulp.watch('components/sass/*.scss', ['compass']);
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
 });
 
 gulp.task( 'connect', function() {
@@ -57,5 +66,14 @@ gulp.task( 'connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee','js','compass', 'connect', 'watch']);
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
+});
+
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
